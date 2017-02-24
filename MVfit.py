37ff9491ba976877af993db80d4823b2e4b7ad59
= <nicholasjhaubrich@gmail.com>
2017-02-24 16:47:55 -0500

    Fixed 2-point slope, everything working atm

--- MVfit.py
+++ MVfit.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 def fn(x,e,f,g):
     return e*x[0]+f*x[1]+g*x[2]
 
+#PART 1
+#Import data and calculate fit parameters
 
 rawdata = genfromtxt('testdata.csv',delimiter=',')
 rawdata = rawdata[1:] #remove top row of text
@@ -20,11 +22,14 @@ data = sp.zeros((N,4))
 #Get X coefficients
 #data gets X,Y and transforms it into dX/dt, X, X^2, XY
 for index,x in enumerate(rawdata):
-
+    print(index)
     # For dX/dt, do 2-point slope, except for first and last datapoints
     if index==0:
+        print(x)
+        print(rawdata[index][0])
+        print(rawdata[index+1][0])
         data[index,0]=rawdata[index+1][0] - rawdata[index][0] 
-    if index==N-1:
+    elif index==N-1:
         data[index,0]=rawdata[index][0] - rawdata[index-1][0]
     else:
         data[index,0]=(rawdata[index+1][0] - rawdata[index-1][0])/2
@@ -33,6 +38,7 @@ for index,x in enumerate(rawdata):
     data[index,3]=x[0]*x[1] #XY
 
 print(data)
+print("\n\n\n\n\n")
 xdata = sp.array(data)
 dx = sp.array(xdata[:,0])
 xdata = sp.transpose(xdata[:,1:])
@@ -47,7 +53,7 @@ for index,x in enumerate(rawdata):
     # For dY/dt, do 2-point slope, except for first and last datapoints
     if index==0:
         data[index,0]=rawdata[index+1][1] - rawdata[index][1] 
-    if index==N-1:
+    elif index==N-1:
         data[index,0]=rawdata[index][1] - rawdata[index-1][1]
     else:
         data[index,0]=(rawdata[index+1][1] - rawdata[index-1][1])/2
@@ -55,12 +61,11 @@ for index,x in enumerate(rawdata):
     data[index,2]=x[1]**2 #Y^2
     data[index,3]=x[0]*x[1] #XY
 
+print(data)
 #Put ydata into scipy array
 ydata = sp.array(data)
 dy = sp.array(ydata[:,0]) #Get dY/dt values from first column
 ydata = sp.transpose(ydata[:,1:]) #Transpose ydata for curve_fit
-
-
 
 ypopt, ypcov = curve_fit(fn,ydata,dy)#Fit parameters for Y,Y^2,XY to dY/dT
 
@@ -86,12 +91,8 @@ M = [xpopt[1],ypopt[1]]
 a = [-1*xpopt[2],-1*ypopt[2]]
 
 
-
-
-
-
-
-#Now plug coefficients into dif-eq and solve it
+#PART 2
+#Plug coefficients into dif-eq and solve it
 
 def species(y,t):   #f0, f1 = dX/dt, dY/dt;   r,M,a are [rx,ry], [Mx,My], etc
     f0 = r[0]*y[0]*(1-y[0]/M[0])-a[0]*y[0]*y[1]
@@ -101,10 +102,9 @@ def species(y,t):   #f0, f1 = dX/dt, dY/dt;   r,M,a are [rx,ry], [Mx,My], etc
 #Get IC from original data
 X0=rawdata[0][0]
 Y0=rawdata[0][1]
-t = np.linspace(0,N,N) #number of months to simulate
+t = np.linspace(0,N,10*N) #number of months to simulate
 
 soln = odeint(species, [X0,Y0],t)#solve ODEs
-print(soln)
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
@@ -112,9 +112,13 @@ ax1 = fig.add_subplot(111)
 ax1.plot(t,soln[:, 0], c='b', label='Fit X')
 ax1.plot(t,soln[:, 1], c='r', label='Fit Y')
 
+ax1.plot(range(N),np.transpose(xdata)[:,0], c='c', label='Data X')
+ax1.plot(range(N),np.transpose(ydata)[:,0], c='g', label='Data Y')
 
-ax1.plot(range(N),rawdata[:,0], c='c', label='Data X')
-ax1.plot(range(N),rawdata[:,1], c='g', label='Data Y')
+
+
+#ax1.plot(range(N),rawdata[:,0], c='c', label='Data X')
+#ax1.plot(range(N),rawdata[:,1], c='g', label='Data Y')
 
 #make parameters human readable
 textstring = ''
