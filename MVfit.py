6bbbd8b1cd22cd87cd3ab3faecb26911eb0776b7
Nicholas J Haubrich <haubrich.n@d25rr0rfgg7v.nunet.neu.edu>
2017-02-24 15:38:32 -0500

    Removed manual guess

--- MVfit.py
+++ MVfit.py
@@ -9,21 +9,14 @@ def fn(x,e,f,g):
     return e*x[0]+f*x[1]+g*x[2]
 
 
-rawdata = genfromtxt('exercisecake.csv',delimiter=',')
+rawdata = genfromtxt('testdata.csv',delimiter=',')
 N = len(rawdata)-2
 #data gets X,Y and transforms it into dX/dt, X, X^2, XY
-
-#guess values for r,M,a here
-r1, r2 = 10,10
-M1, M2 = 1000,8500
-a1, a2 = 30,6
-
-guess = [[r1, M1, a1],[r2,M2,a2]]
 
 data = sp.zeros((N,4))
 
 #Get X coefficients
-#gross but works
+
 for index,x in enumerate(rawdata[1:-1]):
     data[index,0]=rawdata[index+2][0] - rawdata[index+1][0] #dX/dt
     data[index,1]=x[0] #X
@@ -35,7 +28,7 @@ xdata = sp.array(data)
 dx = sp.array(xdata[:,0])
 xdata = sp.transpose(xdata[:,1:])
 
-xpopt, xpcov = curve_fit(fn,xdata,dx, p0=guess[0]) #Fit for dX/dt parameters
+xpopt, xpcov = curve_fit(fn,xdata,dx) #Fit for dX/dt parameters
 
 newdx = fn(xdata,xpopt[0],xpopt[1],xpopt[2])#Calculate dY/dT from new parameters
 
@@ -51,19 +44,20 @@ ydata = sp.array(data)
 dy = sp.array(ydata[:,0])
 ydata = sp.transpose(ydata[:,1:])
 
-ypopt, ypcov = curve_fit(fn,ydata,dy,p0=guess[1])#,bounds=[(0,np.inf),(0,np.inf),(np.inf)]) #Fit for dY/dt parameters
+ypopt, ypcov = curve_fit(fn,ydata,dy)#,bounds=[(0,np.inf),(0,np.inf),(np.inf)]) #Fit for dY/dt parameters
 
 newdy = fn(data,ypopt[0],ypopt[1],ypopt[2]) #Calculate dY/dT from new parameters
 
-#turn values into recognizable coefficients
+#turn values into recognizable coefficients in the form [r1, k1, a1]
 xpopt[1]= -1*xpopt[0]/xpopt[1]
 ypopt[1]= -1*ypopt[0]/ypopt[1]
 
-print xpopt #in the form [r1, k1, a1]
+print xpopt
 print ypopt
-#print "\n"
+
+#standard deviations
 stdx = np.sqrt(np.diag(xpcov))
-stdy = np.sqrt(np.diag(ypcov)) #standard deviations
+stdy = np.sqrt(np.diag(ypcov)) 
 
 
 
@@ -71,16 +65,19 @@ stdy = np.sqrt(np.diag(ypcov)) #standard deviations
 error= sum(np.square(np.divide(np.concatenate([stdx,stdy]),np.concatenate([xpopt,ypopt]))))**(.5)
 print "Error: "+str(error)
 
-
 r = [xpopt[0],ypopt[0]]
 M = [xpopt[1],ypopt[1]]
 a = [-1*xpopt[2],-1*ypopt[2]]
 
-#print r,M,a
 
-#time to solve it
 
-def species(y,t):#f0, f1 = dX/dt, dY/dt, r,M,a are [rx,ry], [Mx,My], etc
+
+
+
+
+#Now plug coefficients into dif-eq and solve it
+
+def species(y,t):   #f0, f1 = dX/dt, dY/dt;   r,M,a are [rx,ry], [Mx,My], etc
     f0 = r[0]*y[0]*(1-y[0]/M[0])-a[0]*y[0]*y[1]
     f1 = r[1]*y[1]*(1-y[1]/M[1])-a[1]*y[0]*y[1]
     return [f0,f1]
@@ -88,9 +85,9 @@ def species(y,t):#f0, f1 = dX/dt, dY/dt, r,M,a are [rx,ry], [Mx,My], etc
 #Get IC from original data
 X0=rawdata[1][0]
 Y0=rawdata[1][1]
-t = np.linspace(0,N-1,100*N)#number of months to simulate
+t = np.linspace(0,N-1,100*N) #number of months to simulate
 
-soln = odeint(species, [X0,Y0],t)#solve ODEs, SUCK IT MATLAB!
+soln = odeint(species, [X0,Y0],t)#solve ODEs
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
